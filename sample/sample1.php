fe6cabeab82e3c4938d9bf198125ad2480c0ef6f
Alberto Arena <arena.alberto@gmail.com>
2015-03-01 16:52:26 +0000

    Update sample

--- sample/sample1.php
+++ sample/sample1.php
@@ -8,9 +8,9 @@ use \Fluidity\FluidizerDefault;
 FluidizerDefault::create();
 
 // Test class
-class Name extends \Fluidity\Fluid\Base
+class Person extends \Fluidity\Fluid\Base
 {
-    protected $name, $surname, $address;
+    protected $person, $surname, $address;
 
     public function __construct()
     {
@@ -30,56 +30,56 @@ class Name extends \Fluidity\Fluid\Base
     }
 }
 
-$name = new Name();
-echo 'name.json: ' . $name->json() . "\n";
+$person = new Person();
+echo 'name.json: ' . $person->json() . "\n";
 
 // Set properties
-$name->name = 'Alberto';
-$name->surname = 'Alberto';
-$name->age = 41;
-$name->address = array('street' => '55 Farringdon Road', 'city' => 'London', 'zip' => 'EC1 000');
-$name->dateBirth = new DateTime();
+$person->name = 'Alberto';
+$person->surname = 'Alberto';
+$person->age = 41;
+$person->address = array('street' => '55 Farringdon Road', 'city' => 'London', 'zip' => 'EC1 000');
+$person->dateBirth = new DateTime();
 
-echo 'name.name: ' . $name->name . "\n";
-echo 'name.surname: ' . $name->surname . "\n";
-echo 'name.age: ' . $name->age . "\n";
-echo 'name.address: ' . $name->address . "\n";
-echo 'name.address[street]: ' . $name->address['street'] . "\n";
+echo 'name.name: ' . $person->name . "\n";
+echo 'name.surname: ' . $person->surname . "\n";
+echo 'name.age: ' . $person->age . "\n";
+echo 'name.address: ' . $person->address . "\n";
+echo 'name.address[street]: ' . $person->address['street'] . "\n";
 
 // Apply fluid method
-echo 'name.json: ' . $name->json() . "\n";
+echo 'name.json: ' . $person->json() . "\n";
 
 // Add a property at runtime
-$name->phone = '020312345678';
-echo 'name.json: ' . $name->json() . "\n";
+$person->phone = '020312345678';
+echo 'name.json: ' . $person->json() . "\n";
 
 // Apply fluid method to a property
-echo 'name.address.json: ' . $name->address->json() . "\n";
+echo 'name.address.json: ' . $person->address->json() . "\n";
 
-foreach ($name as $property => $value) {
+foreach ($person as $property => $value) {
     echo '[loop] name.' . $property . ': ' . $value . ' [' . $value->type() . ':' . $value->length() . ']' . "\n";
 }
 
 // Length
-echo 'name.length: ' . $name->length() . "\n";
-echo 'name.name.length: ' . $name->name->length() . "\n";
-echo 'name.surname.length: ' . $name->surname->length() . "\n";
-echo 'name.age.length: ' . $name->age->length() . "\n";
-echo 'name.phone.length: ' . $name->phone->length() . "\n";
-echo 'name.address.length: ' . $name->address->length() . "\n";
-echo 'name.address[street]: ' . $name->address['street'] . "\n";
-echo 'name.address[street].length: ' . $name->address['street']->length() . "\n";
+echo 'name.length: ' . $person->length() . "\n";
+echo 'name.name.length: ' . $person->name->length() . "\n";
+echo 'name.surname.length: ' . $person->surname->length() . "\n";
+echo 'name.age.length: ' . $person->age->length() . "\n";
+echo 'name.phone.length: ' . $person->phone->length() . "\n";
+echo 'name.address.length: ' . $person->address->length() . "\n";
+echo 'name.address[street]: ' . $person->address['street'] . "\n";
+echo 'name.address[street].length: ' . $person->address['street']->length() . "\n";
 
 // Call native method
-$name->surname = 'Smith';
-echo 'name.label1: ' . $name->label1() . "\n";
-echo 'name.label2: ' . $name->label2() . "\n";
+$person->surname = 'Smith';
+echo 'name.label1: ' . $person->label1() . "\n";
+echo 'name.label2: ' . $person->label2() . "\n";
 
 // Call injected methods
-$name->name = 'Jim';
-$name['surname'] = 'Jones';
-$name->sayHello = function ($obj) {
+$person->name = 'Jim';
+$person['surname'] = 'Jones';
+$person->sayHello = function ($obj) {
     echo 'Hello, I am ' . $obj['name'] . ' ' . $obj['surname'] . "\n";
 };
-$name->sayHello();
-echo "name.sayHello.type: " . $name->sayHello->type() . "\n";
+$person->sayHello();
+echo "name.sayHello.type: " . $person->sayHello->type() . "\n";
